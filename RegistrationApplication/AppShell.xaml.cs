106dfd6c34cfb0485470827f19eb059e39dd647d
iam-luh <lusungugodwin20@gmail.com>
2023-08-10 16:44:07 +0300

    Add project files.

---
+++ RegistrationApplication/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿using RegistrationApplication.Pages;
+namespace RegistrationApplication;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(HomePage),typeof(HomePage));
+	}
+}
---
+++ RegistrationApplication/Pages/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+namespace RegistrationApplication.Pages;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+
+    private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void checkboxrecurring_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void btnSend_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btndraft_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
